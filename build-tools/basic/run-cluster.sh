9ae55780df6040e0fef621d2064a6559ad21e006
Gerald Humphries <geraldhumphries@gmail.com>
2023-03-15 12:07:49 -0400

    Update run-cluster script to use tilt-helm-values.yaml
    Fix HS-1220

--- build-tools/basic/run-cluster.sh
+++ build-tools/basic/run-cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-helm upgrade -i opennms ./charts/opennms -f build-tools/basic/helm-values.yaml \
+helm upgrade -i opennms-basic ./charts/opennms -f tilt-helm-values.yaml \
   --set Grafana.Image=opennms/horizon-stream-grafana-dev:local-basic \
   --set Keycloak.Image=opennms/horizon-stream-keycloak-dev:local-basic \
   --set OpenNMS.API.Image=opennms/horizon-stream-rest-server:local-basic \
